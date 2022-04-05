b832048a11f95b4ff3bde83811ad1db5ebcd4090
S-W-R <sn_swr1@mail.ru>
2022-04-05 11:57:03 +0500

    homework_6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,48 @@
 object Exercises {
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.foldLeft(Nil: Seq[T])((acc, v) => v +: acc)
 
+  def hylo[A, B](coalgebra: A => Seq[A], algebra: Seq[B] => B)(a: A): B =
+    algebra(coalgebra(a).map(hylo(coalgebra, algebra)))
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case x if x > 0 => hylo[Int, Int](
+      coalgebra = x => x match {
+        case x if x > 2 => x - 1 :: x - 2 :: Nil
+        case _ => Nil
+      },
+      algebra = x => x.sum match {
+        case x if x > 0 => x
+        case _ => 1
+      })(idx)
+    case _ => 0
+  }
 
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
-
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = Range.inclusive(0, idx).map(fibonacci4Index)
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text.map(x => MORSE.getOrElse(x.toUpper.toString, x)).mkString(" ")
 
+  val wordParser = "\\w+".r
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = {
+    def reverse(string: String): String = string.headOption.map(head => head match {
+      case h if h.isUpper => string.tail.reverse.capitalize + h.toLower
+      case _ => string.reverse
+    }).getOrElse("")
 
+    text.foldLeft(("", ""))((acc, v) => acc match {
+      case (res, acc) => v match {
+        case v if v.isLetter => (res, acc + v)
+        case v => (res + reverse(acc) + v, "")
+      }
+    }
+    ) match {
+      case (res, acc) => res + acc
+    }
+  }
 }
