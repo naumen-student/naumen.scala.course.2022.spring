f1f3b09eebd20ea3b80b2478d0d8cdf6015eb280
Denis Bautin <denisbduis@gmail.com>
2022-04-06 20:55:55 +0500

    Done homework 6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,11 @@
+import scala.util.matching.Regex
+
 object Exercises {
 
-
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq match {
+    case head :: tail => reverse(tail) ++ Seq(head)
+    case Nil => seq
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +13,17 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case 0 => 0
+    case 1 => 1
+    case _ => fibonacci4Index(idx - 2) + fibonacci4Index(idx - 1)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = idx match {
+    case 0 => Seq(fibonacci4Index(idx))
+    case 1 => Seq(fibonacci4Index(0), fibonacci4Index(1))
+    case _ => fibonacci(idx - 1) :+ fibonacci4Index(idx)
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +31,21 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text
+    .map(char => MORSE.getOrElse(char.toString.toUpperCase(), char))
+    .mkString(" ")
 
+  def wordReverse(text: String): String = {
+    val pattern = new Regex("\\pL+")
 
-  def wordReverse(text: String): String = ???
+    pattern.replaceAllIn(text, wordMatch => {
+      val reversedWord = wordMatch.toString.reverse
+
+      if (reversedWord.last.isUpper)
+        reversedWord.toLowerCase.capitalize
+      else
+        reversedWord
+    })
+  }
 
 }
