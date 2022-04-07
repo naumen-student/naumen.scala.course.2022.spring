3832fe87afe01ef1d4f3a6efc81848518102e0d9
Stepan Khudyakov <sah@renue.ru>
2022-04-07 21:10:24 +0500

    Done homework 6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,9 @@
+import scala.util.matching.Regex
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,19 +11,35 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    idx match {
+      case 0 => 0
+      case 1 => 1
+      case _ => fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
+    }
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    idx match {
+      case 0 => Seq(0)
+      case _ => fibonacci(idx - 1) :+ fibonacci4Index(idx)
+    }
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text
+    .map(c => MORSE.getOrElse(c.toUpper.toString, c.toString))
+    .mkString(" ")
 
+  val wordRegex: Regex = "([а-яА-ЯёЁa-zA-Z]+|[^а-яА-ЯёЁa-zA-Z])".r
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = wordRegex.findAllIn(text)
+    .map(word => if (word.charAt(0).isUpper) word.toLowerCase.reverse.capitalize else word.reverse)
+    .mkString("")
 
 }
