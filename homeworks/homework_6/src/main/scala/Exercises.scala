adbf36072e5c67039b28d6118e01629da23f16aa
Тиунов Владимир <vova_tiunov@list.ru>
2022-04-08 02:46:33 +0500

    homework_6 - Тиунов Владимир

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,14 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
+  val fibStream: Stream[Int] = 0 #:: 1 #:: fibStream.zip(fibStream.tail).map(n => n._1 + n._2)
+
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.foldLeft(Seq[T]())((tail, el) => el +: tail)
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,19 +16,17 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci4Index(idx: Int): Int = fibStream(idx)
 
-  lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+  def fibonacci(idx: Int): Seq[Int] = fibStream.take(idx + 1)
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text.map(ch => MORSE.getOrElse(ch.toUpper.toString, ch)).mkString(" ")
 
-
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String =
+    "([а-яА-Яa-zA-Z]+|[^а-яА-Яa-zA-Z])".r.findAllIn(text).map(word =>
+      if (word.head.isUpper) word.toLowerCase.reverse.capitalize
+      else word.reverse
+    ).mkString("")
 
 }
