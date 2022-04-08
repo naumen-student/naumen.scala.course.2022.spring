709fd7a7b68cc2d2f6af5316690914d6fabee1ae
Максим Францевич <FranzuaAirMax@yandex.ru>
2022-04-08 16:18:42 +0500

    homework_5 - Францевич Максим

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -9,15 +9,32 @@ object Exercises {
 
 
 
-  case class Shelter ...
+  case class Shelter[+T <: Animal](animals: List[Animal]) {
+    def +[T2 <: Animal](animal: T2): Shelter[T2] = Shelter(animals :+ animal)
 
+    def ++(shelter: Shelter[Animal]): Shelter[Animal] = Shelter(animals ++ shelter.animals)
 
+    def getNames: List[String] = {
+      animals.map(_.name)
+    }
 
-  trait Food ...
+    def feed(food: Food[Animal]): List[String] = {
+      animals.map(food.feed)
+    }
+  }
 
-  case object Meat extends Food[Animal] ...
+  trait Food[+T <: Animal] {
+    private val name = this.getClass.getSimpleName.toLowerCase
+    private val nameWithoutLastChar = name.substring(0, name.length - 1)
 
-  case object Milk extends Food[Cat] ...
+    def feed(animal: Animal): String = {
+      s"${animal.name} eats $nameWithoutLastChar"
+    }
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Meat extends Food[Animal]
+
+  case object Milk extends Food[Cat]
+
+  case object Bread extends Food[Dog]
 }
