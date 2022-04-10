a1f88ca91060fd61ea846959acd5c999ce5b9809
Stanislav Ilin <youngfrezyx@gmail.com>
2022-04-10 21:29:45 +0500

    homework_6 - Ильин Станислав

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,41 @@
+import scala.util.matching.Regex
+
 object Exercises {
 
-
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
-
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
-
-  def fibonacci(idx: Int): Seq[Int] = ???
-
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq match {
+    case head :: tail => reverse(tail) ++ Seq(head)
+    case Nil => seq
+  }
 
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case 0 => 0
+    case 1 => 1
+    case _ => fibonacci4Index(idx - 2) + fibonacci4Index(idx - 1)
+  }
 
-  def wordReverse(text: String): String = ???
+  def fibonacci(idx: Int): Seq[Int] = idx match {
+    case 0 => Seq(fibonacci4Index(idx))
+    case 1 => Seq(fibonacci4Index(0), fibonacci4Index(1))
+    case _ => fibonacci(idx - 1) :+ fibonacci4Index(idx)
+  }
 
+  def morse(text: String): String = text
+    .map(char => MORSE.getOrElse(char.toString.toUpperCase(), char))
+    .mkString(" ")
+
+  def wordReverse(text: String): String = {
+    new Regex("\\pL+").replaceAllIn(text, w => {
+      val reversed = w.toString().reverse
+      if (reversed.last.isUpper)
+        reversed.toLowerCase.capitalize
+      else
+        reversed
+    })
+  }
 }
