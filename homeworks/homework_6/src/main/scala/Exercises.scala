0165a2389ff21d27832b9ecfb7f885aac9248cb5
Tomorrow23 <44552112+Tomorrow23@users.noreply.github.com>
2022-04-15 17:29:57 +0500

    hw6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,7 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +9,17 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case 0 => 0
+    case 1 => 1
+    case i => fibonacci4Index(i - 1) + fibonacci4Index(i - 2)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = idx match {
+    case 0 => Seq(fibonacci4Index(idx))
+    case 1 => Seq(fibonacci4Index(0), fibonacci4Index(1))
+    case i => fibonacci(idx - 1) :+ fibonacci4Index(idx)
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +27,12 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text.map(char => MORSE.getOrElse(char.toString.toUpperCase(), char)).mkString(" ")
 
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String =  "([а-яА-Яa-zA-Z]+|[^а-яА-Яa-zA-Z])".r.findAllIn(text).map(word =>
+    if (word.head.isUpper) word.toLowerCase.reverse.capitalize
+    else word.reverse
+  ).mkString("")
 
 }
