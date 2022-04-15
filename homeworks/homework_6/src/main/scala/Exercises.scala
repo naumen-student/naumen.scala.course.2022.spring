7a9d62cf12cbfd5036d8b057c0f794de3290ff8a
Максим Францевич <FranzuaAirMax@yandex.ru>
2022-04-15 23:03:21 +0500

    homework_6 - Францевич Максим

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,9 @@
+import scala.annotation.tailrec
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,19 +11,56 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci4Index(idx: Int, fibonacciSeq: Seq[Int] = Seq(0, 1)): Int = {
+    fibonacci(idx, fibonacciSeq).last
+  }
+
+  @tailrec
+  def fibonacci(idx: Int, fibonacciSeq: Seq[Int] = Seq(0, 1)): Seq[Int] = {
+    if (idx < 0) {
+      throw new IllegalArgumentException
+    } else if (fibonacciSeq.length < 2) {
+      throw new IllegalArgumentException
+    } else if (idx < fibonacciSeq.length) {
+      fibonacciSeq.slice(0, idx + 1)
+    } else {
+      fibonacci(idx, fibonacciSeq.appended(fibonacciSeq.last + fibonacciSeq(fibonacciSeq.length - 2)))
+    }
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = {
+    text.flatMap(char => {
+      val upperChar = char.toUpper.toString
+      if (MORSE.contains(upperChar)) {
+        f" ${MORSE(upperChar)}"
+      } else if (char.isSpaceChar) {
+        f" $char"
+      } else {
+        char.toString
+      }
+    }).substring(1)
+  }
 
-
-  def wordReverse(text: String): String = ???
-
+  @tailrec
+  def wordReverse(text: String, leftText: String = ""): String = {
+    if (text.isEmpty) {
+      return leftText
+    }
+    val spanText = text.span(_.isLetter)
+    val firstWord = spanText._1.reverse
+    val upperFirstWord = if (firstWord.last.isUpper) {
+      f"${firstWord.head.toUpper}${firstWord.toLowerCase.substring(1)}"
+    } else {
+      firstWord
+    }
+    val secondSpanText = spanText._2.span(!_.isLetter)
+    wordReverse(secondSpanText._2, leftText + upperFirstWord + secondSpanText._1)
+  }
 }
