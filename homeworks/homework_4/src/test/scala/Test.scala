fc34bbc53ccdeeace7adcb74d5ad1492080caad8
xxeniaev <xenia-ev2502@yandex.ru>
2022-04-19 17:38:00 +0500

    homework_4 - Евдокимова Ксения

--- homeworks/homework_4/src/test/scala/Test.scala
+++ homeworks/homework_4/src/test/scala/Test.scala
@@ -5,12 +5,12 @@ object Test extends TestSuite {
         'test_createTable - {
             val table = new Table(3, 3)
             for (i <- 0 until 9) {
-                assert(table.getCell(i / 3, i % 3).map(_.toString) == Some("empty"))
+                assert(table.getCell(i / 3, i % 3).map(_.toString).contains("empty"))
             }
-            assert(table.getCell(0, -1).map(_.toString) == None)
-            assert(table.getCell(-1, 0).map(_.toString) == None)
-            assert(table.getCell(9, 8).map(_.toString) == None)
-            assert(table.getCell(8, 9).map(_.toString) == None)
+            assert(table.getCell(0, -1).map(_.toString).isEmpty)
+            assert(table.getCell(-1, 0).map(_.toString).isEmpty)
+            assert(table.getCell(9, 8).map(_.toString).isEmpty)
+            assert(table.getCell(8, 9).map(_.toString).isEmpty)
         }
         'test_numberCell - {
             val table = new Table(2, 2)
@@ -18,36 +18,36 @@ object Test extends TestSuite {
             val cellInt11 = new NumberCell(2147483647)
             table.setCell(0, 0, cellInt00)
             table.setCell(1, 1, cellInt11)
-            assert(table.getCell(0, 0).map(_.toString) == Some("5"))
-            assert(table.getCell(0, 1).map(_.toString) == Some("empty"))
-            assert(table.getCell(1, 0).map(_.toString) == Some("empty"))
-            assert(table.getCell(1, 1).map(_.toString) == Some("2147483647"))
+            assert(table.getCell(0, 0).map(_.toString).contains("5"))
+            assert(table.getCell(0, 1).map(_.toString).contains("empty"))
+            assert(table.getCell(1, 0).map(_.toString).contains("empty"))
+            assert(table.getCell(1, 1).map(_.toString).contains("2147483647"))
         }
         'test_stringCell - {
             val table = new Table(2, 2)
-            val cellStr01 = new StringCell("01")
-            val cellStr10 = new StringCell("10")
+            val cellStr01 = StringCell("01")
+            val cellStr10 = StringCell("10")
             table.setCell(0, 1, cellStr01)
             table.setCell(1, 0, cellStr10)
-            assert(table.getCell(0, 0).map(_.toString) == Some("empty"))
-            assert(table.getCell(0, 1).map(_.toString) == Some("01"))
-            assert(table.getCell(1, 0).map(_.toString) == Some("10"))
-            assert(table.getCell(1, 1).map(_.toString) == Some("empty"))
+            assert(table.getCell(0, 0).map(_.toString).contains("empty"))
+            assert(table.getCell(0, 1).map(_.toString).contains("01"))
+            assert(table.getCell(1, 0).map(_.toString).contains("10"))
+            assert(table.getCell(1, 1).map(_.toString).contains("empty"))
         }
         'test_referenceCell - {
             val table = new Table(3, 3)
             /*ix = 0*/
-            val cellStr00 = new StringCell("00")
-            val cellRef01 = new ReferenceCell(0, 2, table)
-            val cellRef02 = new ReferenceCell(0, 1, table)
+            val cellStr00 = StringCell("00")
+            val cellRef01 = ReferenceCell(0, 2, table)
+            val cellRef02 = ReferenceCell(0, 1, table)
             /*ix = 1*/
-            val cellInt10 = new NumberCell(10)
-            val cellInt11 = new NumberCell(11)
-            val cellRef12 = new ReferenceCell(0, 0, table)
+            val cellInt10 = NumberCell(10)
+            val cellInt11 = NumberCell(11)
+            val cellRef12 = ReferenceCell(0, 0, table)
             /*ix = 2*/
-            val cellEmpty20 = new EmptyCell
-            val cellRef21 = new ReferenceCell(1, 1, table)
-            val cellRef22 = new ReferenceCell(2, 1, table)
+            val cellEmpty20 = EmptyCell()
+            val cellRef21 = ReferenceCell(1, 1, table)
+            val cellRef22 = ReferenceCell(2, 1, table)
             table.setCell(0, 0, cellStr00)
             table.setCell(0, 1, cellRef01)
             table.setCell(0, 2, cellRef02)
