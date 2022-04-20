42de17ff795db3a6a6ac6ead75d5dd61e5df6179
Banan-dot <semke.danya@mail.ru>
2022-04-21 00:20:48 +0500

    Refactor

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,34 +1,35 @@
 import scala.util.{Failure, Success, Try}
 object Exercises {
-  trait Read[T]{
-    def read(value: String): Either[String, T];
+
+  private val somePattern = "Some\\((.+)\\)".r
+
+  trait Read[T] {
+    def read(value: String): Either[String, T]
   }
 
-  object Read{
-    def read[T](value: String)(implicit read: Read[T]): Either[String, T]  = read.read(value);
+  implicit class ReadOps(value: String) {
+    def read[T](implicit reader: Read[T]) = reader.read(value)
   }
 
-  implicit class ReadOps(value: String){
-    def read[T](implicit reader: Read[T]): Either[String, T]  = reader.read(value);
-  }
-
-  implicit val stringRead: Read[String] = (value: String) => Right(value)
+  implicit val stringRead: Read[String] = Right(_)
 
 
-  implicit val intRead: Read[Int] = (value: String) => {
+  implicit val intRead: Read[Int] = value => {
     Try(value.toInt) match {
       case Success(x) => Right(x)
       case Failure(_) => Left(value)
     }
   }
 
-  private val somePattern = "Some\\((.+)\\)".r
+  object Read {
+    def read[T](value: String)(implicit read: Read[T]) = read.read(value)
+  }
 
   implicit def optionRead[T: Read](implicit reader: Read[T]): Read[Option[T]] = {
     case "None" => Right(None);
     case value => somePattern.findFirstMatchIn(value) match {
-      case Some(i) => reader.read(i.group(1)).toOption match {
-        case Some(i) => Right(Some(i))
+      case Some(str) => reader.read(str.group(1)).toOption match {
+        case Some(x) => Right(Some(x))
         case None => Left(value);
       };
       case None => Left(value);
