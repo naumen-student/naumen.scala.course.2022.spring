064b5fb773af89f46537d1c1b64980c4214c0f01
DaGnYMilleR <danil.lunyow@mail.ru>
2022-04-22 23:50:08 +0500

    done

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,36 @@
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 object Exercises {
+  trait Read[T] {
+    def read(str: String): Either[String, T]
+  }
+
+  implicit class ReadOps(value: String) {
+    def read[T](implicit reader: Read[T]): Either[String, T] = reader.read(value)
+  }
+
+  object Read {
+    def read[T](value: String)(implicit read: Read[T]): Either[String, T] = read.read(value)
+  }
+
+  implicit val stringRead: Read[String] = Right(_)
+
+  implicit val intReader: Read[Int] = (value: String) => {
+    Try(value.toInt) match {
+      case Success(x) => Right(x)
+      case Failure(_) => Left(value)
+    }
+  }
+
+  private val somePattern = "Some\\((.+)\\)".r
+
+  implicit def optionRead[T: Read](implicit reader: Read[T]): Read[Option[T]] = {
+    case "None" => Right(None);
+    case value => somePattern.findFirstMatchIn(value) match {
+      case Some(str) => reader.read(str.group(1)).toOption match {
+        case Some(x) => Right(Some(x))
+        case None => Left(value);
+      };
+      case None => Left(value);
+    }
+  }
 }
