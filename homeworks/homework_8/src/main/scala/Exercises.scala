71068d0822acee0b0423240bcfa6e0f9a2fb92d7
Максим Францевич <FranzuaAirMax@yandex.ru>
2022-04-22 15:49:02 +0500

    Homework_8 - Францевич Максим

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,26 @@
-import scala.util.Try
+import scala.annotation.unused
+
 object Exercises {
+  trait Read[T] { def read(@unused string: String): Either[String, T] }
+
+  implicit val stringRead: Read[String] = (string: String) => Right(string)
+
+  implicit val intRead: Read[Int] = (string: String) => {
+    val value = string.toIntOption
+    if (value.isEmpty) Left("")
+    else Right(value.get)
+  }
+
+  implicit def optionRead[T: Read]: Read[Option[T]] = (string: String) => {
+    if (string == "None") Right(None)
+    else if (string.startsWith("Some(") && string.endsWith(")") && string.length >= 7)
+      string.substring(5, string.length - 1).read[T].map(a => Some(a))
+    else Left("")
+  }
+
+  object Read { def read[T](string: String)(implicit read: Read[T]): Either[String, T] = read.read(string) }
+
+  implicit class ReadOps(string: String) {
+    def read[T]()(implicit read: Read[T]): Either[String, T] = read.read(string)
+  }
 }
