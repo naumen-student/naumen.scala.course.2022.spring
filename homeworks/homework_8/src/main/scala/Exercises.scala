150b77d392c1abff362ee7ee62b31b1e6f94d6a3
Тиунов Владимир <vova_tiunov@list.ru>
2022-04-27 21:37:19 +0500

    homework_8 - Тиунов Владимир

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,38 @@
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 object Exercises {
+
+  trait Read[T] {
+    def read(str: String): Either[String, T]
+  }
+
+  object Read {
+    def read[T](v: String)(implicit read: Read[T]): Either[String, T] = read.read(v)
+  }
+
+  implicit class ReadOps(v: String) {
+    def read[T](implicit reader: Read[T]): Either[String, T] = reader.read(v)
+  }
+
+  implicit val stringReader: Read[String] = Right(_)
+
+  implicit val intReader: Read[Int] = (v: String) => {
+    Try(v.toInt) match {
+      case Success(num) => Right(num)
+      case Failure(_) => Left("Value can not be converted to Int")
+    }
+  }
+
+  implicit def optionReader[T: Read](implicit reader: Read[T]): Read[Option[T]] = {
+    case "None" => Right(None)
+    case v => OptionSomeReg.findFirstMatchIn(v) match {
+      case Some(inside1) => reader.read(inside1.group(1)).toOption match {
+        case Some(inside2) => Right(Some(inside2))
+        case None => Left(v)
+      }
+      case None => Left(v)
+    }
+  }
+
+  private val OptionSomeReg = "Some\\((.+)\\)".r
+
 }
