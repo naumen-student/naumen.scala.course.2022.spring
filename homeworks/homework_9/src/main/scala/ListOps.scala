64b8329f64815fa6ed975fe5bb31f137a2948b0e
gre-ilya <ilya.grebenyuk.01@mail.ru>
2022-05-06 16:53:28 +0500

    done

--- homeworks/homework_9/src/main/scala/ListOps.scala
+++ homeworks/homework_9/src/main/scala/ListOps.scala
@@ -1,3 +1,5 @@
+import DataList.{EmptyList, NonEmptyList}
+
 import scala.annotation.tailrec
 
 object ListOps {
@@ -6,31 +8,49 @@ object ListOps {
    * Функция fold "сворачивает" список из Т в один элемент типа Т.
    * Если в списке лишь один элемент, то он и вернётся, два - вернётся результат применения f к этим элементам,
    * больше двух - результат применения к f(f(f(...), a[i - 1]), a[i])
+   *
    * @param f функция свёртывания. Применяется попарно к предыдущему результату применения и i-ому элементу списка
    * @return None - если список пустой
    */
-  def foldOption[T](f: (T, T) => T): DataList[T] => Option[T] = ???
-
+  def foldOption[T](f: (T, T) => T): DataList[T] => Option[T] = {
+    case NonEmptyList(head, tail) => foldOption(f)(tail) match {
+      case Some(value) => Some(f(head, value))
+      case None => Some(head)
+    }
+    case EmptyList => None
+  }
 
   /**
    * Используя foldOption[T](f: (T, T) => T) реализуйте суммирование всех элементов списка.
+   *
    * @return Если список пустой, то 0
    */
-  def sum[T : Numeric](list: DataList[T]): T = {
+  def sum[T: Numeric](list: DataList[T]): T = {
     /**
      * Используйте для суммирования двух чисел любого типа (Int, Long, Double, Float etc)
      */
     def sumT(a: T, b: T) = implicitly[Numeric[T]].plus(a, b)
 
-    ???
+    foldOption(sumT)(list).getOrElse(implicitly[Numeric[T]].zero)
   }
 
   /**
    * Фильтрация списка. Хвостовая рекурсия
+   *
    * @param f - фильтрующее правило (если f(a[i]) == true, то элемент остаётся в списке)
    */
   @tailrec
-  private def filterImpl[T](f: T => Boolean)(buffer: DataList[T])(l: DataList[T]): DataList[T] = ???
+  private def filterImpl[T](f: T => Boolean)(buffer: DataList[T])(l: DataList[T]): DataList[T] = l match {
+    case EmptyList => reverse(buffer, EmptyList)
+    case NonEmptyList(head, tail) =>
+      filterImpl(f)(if (f(head)) NonEmptyList(head, buffer) else buffer)(tail)
+  }
+
+  @tailrec
+  def reverse[T](l: DataList[T], buffer: DataList[T]): DataList[T] = l match {
+    case NonEmptyList(head, tail) => reverse(tail, NonEmptyList(head, buffer))
+    case EmptyList => buffer
+  }
 
   final def filter[T](f: T => Boolean): DataList[T] => DataList[T] = filterImpl(f)(DataList.EmptyList)
 
@@ -43,6 +63,6 @@ object ListOps {
    * Используя композицию функций реализуйте collect. Collect - комбинация filter и map.
    * В качестве фильтрующего правила нужно использовать f.isDefinedAt
    */
-  def collect[A, B](f: PartialFunction[A, B]): DataList[A] => DataList[B] = ???
+  def collect[A, B](f: PartialFunction[A, B]): DataList[A] => DataList[B] = filter(f.isDefinedAt).andThen(map(f))
 
 }
